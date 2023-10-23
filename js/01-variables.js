a09dcdb6bc87501b4b6e96ee8ece217de272f54b
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2023-10-23 11:32:59 -0300

    practica con alert y prompt

--- js/01-variables.js
+++ js/01-variables.js
@@ -19,3 +19,23 @@ anioActual = 2023 + 1;
 
 console.log("Año actual: " + anioActual);
 console.log(url);
+
+
+document.write("<h1 class='titulo'>hola mundo "+ anioActual +"</h1>");
+document.write('hola mundo</br>');
+document.write("hola mundo");
+
+// ventana emergente
+alert("Esto es un ejemplo de una alerta")
+
+// Solicitar un numero al usuario y luego sumarle 5, por ultimo mostrar el resultado de la suma
+
+const numeroSolicitado = parseInt(prompt("Ingrese un numero del 1 al 100"));
+console.log(numeroSolicitado);
+
+console.log('Resultado: ' + (numeroSolicitado + 5));
+console.log(numeroSolicitado + 5);
+
+// transformar 0 parsear un numero de string a formato numerico
+// parseInt("100")
+// parseFloat("100.50")
