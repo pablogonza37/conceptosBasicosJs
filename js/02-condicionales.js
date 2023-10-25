4c48875d17bd76cfbdacd1dc201b1806308dc569
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2023-10-25 10:18:14 -0300

    agregamos un else if en el ejercicio

--- js/02-condicionales.js
+++ js/02-condicionales.js
@@ -12,11 +12,9 @@ const edad = parseInt(prompt('Ingrese su edad'));
 
 if(edad >= 16 && edad < 18 || edad > 70){
     alert('Es optativo votar');
-}else{
-    if(edad >= 18 && edad <= 70){
-        alert('Usted debe votar');
-    }else{
-        alert('No puedes votar')
-    }
-    
+} else if(edad >= 18 && edad <= 70){
+    alert('Usted debe votar');
+} else{
+    alert('No puedes votar');
 }
+    
